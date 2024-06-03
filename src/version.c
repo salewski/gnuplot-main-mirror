f5efc9a4a6fdbfd3e3b7abbbbc4b34b671be8c01
Ethan A Merritt <merritt@u.washington.edu>
2024-06-03 12:15:46 -0700

    Bump PATCHLEVEL to 6.0.2

--- src/version.c
+++ src/version.c
@@ -37,11 +37,11 @@
 
 const double gnuplot_ver = 6.0;
 const char gnuplot_version[] = "6.0";
-const char gnuplot_patchlevel[] = "1";
+const char gnuplot_patchlevel[] = "2";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2024-05-13 ";
+const char gnuplot_date[] = "2024-06-03 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2024";
 
