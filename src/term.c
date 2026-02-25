AlexanderTaeschner/gnuplot

expand Unicode escape sequences \U+xxxx in "noenhanced" strings
The unicode escape sequence processing added in version 5.4 was only
applied in "enhanced text" mode.  Now the processing is applied earlier
so that it is in effect whether on not enhanced text mode is active.

It is safest to put a space after the escape code so that a 4-character
code point is not mistaken as a 5-character code point when the following
character is a legal hexdigit.
In enhanced text mode the escape sequence can be wrapped in curly brackets
to avoid this ambiguity
	e.g. write α1+α2 as {\U+03B1}1+{\U+03B1}2
For "noenhance" text you must add a space
	e.g. write α1+α2 as \U+03B1 1+\U+03B1 2

src/term.c
@@ -240,6 +240,7 @@ static void PUTTEXT_null(unsigned int, unsigned int, const char *);
 static TBOOLEAN sanity_check_font_size(void);
 
 static int strlen_tex(const char *);
+static char *expand_unicode_escapes(char *text);
 
 static char *stylefont(const char *fontname, TBOOLEAN isbold, TBOOLEAN isitalic);
 
@@ -792,6 +793,9 @@ write_multiline(
 	    y += (vert * lines * t->v_char) / 2;
     }
 
+    /* Replace unicode escape sequences with utf8 byte sequence */
+    text = expand_unicode_escapes(text);
+
     for (;;) {                  /* we will explicitly break out */
 
 	if ((text != NULL) && (p = strchr(text, '\n')) != NULL)
@@ -837,6 +841,44 @@ write_multiline(
 
 }
 
+/*
+ * Replace unicode escape sequences \U+xxxx with the corresponding
+ * UTF-8 byte sequences.  Note that a utf8 character encoding is no longer than
+ * four bytes, which is always less than the seven character escape sequence,
+ * so the substitution can be done in place.
+ * If the current encoding is not utf8, do nothing.
+ * NB: Returned string must be consumed before this routine is called again
+ *     (static char *out)
+ */
+static char *
+expand_unicode_escapes(char *text)
+{
+    static char *out = NULL;
+    char *p, *rest;
+
+    if (encoding != S_ENC_UTF8)
+	return text;
+    if ((p = strstr(text, "\\U+")) == NULL)
+	return text;
+
+    free(out);
+    p = out = strdup(text);
+
+    while ( (p = strstr(p, "\\U+")) != NULL) {
+	if (!isxdigit(p[3]) || !isxdigit(p[4]) || !isxdigit(p[5]) || !isxdigit(p[6])) {
+	    int_warn(NO_CARET, "misformed unicode escape sequence %7.7s", p);
+	    p += 7;
+	    continue;
+	}
+	rest = (isxdigit(p[7])) ?  &(p[8]) : &(p[7]);
+	truncate_to_one_utf8_char(p);
+	advance_one_utf8_char(p);
+	memcpy(p, rest, strlen(rest)+1);
+    }
+
+    FPRINTF((stderr, "replacing \"%s\" with \"%s\"\n", text, out));
+    return out;
+}
 
 static void
 do_point(unsigned int x, unsigned int y, int number)

salewski/gnuplot-main-mirror

src/term.c
@@ -241,6 +241,7 @@ static void PUTTEXT_null(unsigned int, unsigned int, const char *);
 static TBOOLEAN sanity_check_font_size(void);
 
 static int strlen_tex(const char *);
+static char *expand_unicode_escapes(char *text);
 
 static char *stylefont(const char *fontname, TBOOLEAN isbold, TBOOLEAN isitalic);
 
@@ -824,6 +825,9 @@ write_multiline(
 	    y += (vert * lines * t->v_char) / 2;
     }
 
+    /* Replace unicode escape sequences with utf8 byte sequence */
+    text = expand_unicode_escapes(text);
+
     for (;;) {                  /* we will explicitly break out */
 
 	if ((text != NULL) && (p = strchr(text, '\n')) != NULL)
@@ -869,6 +873,44 @@ write_multiline(
 
 }
 
+/*
+ * Replace unicode escape sequences \U+xxxx with the corresponding
+ * UTF-8 byte sequences.  Note that a utf8 character encoding is no longer than
+ * four bytes, which is always less than the seven character escape sequence,
+ * so the substitution can be done in place.
+ * If the current encoding is not utf8, do nothing.
+ * NB: Returned string must be consumed before this routine is called again
+ *     (static char *out)
+ */
+static char *
+expand_unicode_escapes(char *text)
+{
+    static char *out = NULL;
+    char *p, *rest;
+
+    if (encoding != S_ENC_UTF8)
+	return text;
+    if ((p = strstr(text, "\\U+")) == NULL)
+	return text;
+
+    free(out);
+    p = out = strdup(text);
+
+    while ( (p = strstr(p, "\\U+")) != NULL) {
+	if (!isxdigit(p[3]) || !isxdigit(p[4]) || !isxdigit(p[5]) || !isxdigit(p[6])) {
+	    int_warn(NO_CARET, "misformed unicode escape sequence %7.7s", p);
+	    p += 7;
+	    continue;
+	}
+	rest = (isxdigit(p[7])) ?  &(p[8]) : &(p[7]);
+	truncate_to_one_utf8_char(p);
+	advance_one_utf8_char(p);
+	memmove(p, rest, strlen(rest)+1);
+    }
+
+    FPRINTF((stderr, "replacing \"%s\" with \"%s\"\n", text, out));
+    return out;
+}
 
 static void
 do_point(unsigned int x, unsigned int y, int number)